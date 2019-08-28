a984b64c332eccc135eb55fd8363b6658d5db259
xinggevip <1511844263@qq.com>
2019-08-28 11:06:04 +0800

    v-if,v-model,v-show,v-on:click

---
+++ Lesson/Lesson01/index.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+    el:"#app",
+    data:{
+        message:"v-if,v-model",
+        loding:true,
+        scor:10
+    },
+    methods:{
+        fn:function (){
+            alert("提示内容！");
+        }
+    }
+})
+
+
