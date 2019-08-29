e05291708e63c052bcfff602b115779c67a00c2a
xinggevip <1511844263@qq.com>
2019-08-29 21:37:39 +0800

    v-bind属性绑定

---
+++ Lesson/Lesson04/index.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        name:"Tom",
+        url:"http://www.qiangssvip.com",
+        webTag:"<a href='http://www.baidu.com'>百度</a>"
+    },
+    methods: {
+        
+
+    }
+})
+
+
